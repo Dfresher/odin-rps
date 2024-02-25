b2efa219ac9de1134b2c0dd24e67139a7249027c
Donfernando <121210665+Dfresher@users.noreply.github.com>
2024-02-24 23:26:05 -0400

    Add functions to get player and computer choice
    I wanted to make them shorter and more succinct using ternary operators
    but the MDN documentation was too intimidating so I stuck with the ol'
    trustworthy.
    
    Also, this way they're more readable, not a cope at all.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("hello world!")
+function getPlayerChoice() {
+    keepGoing = true;
+    while (keepGoing) {
+        let playerPrompt = prompt("Rock, Paper or Scissors?");
+        if (typeof(playerPrompt) === "string" || playerPrompt instanceof String) {
+            let playerChoice = playerPrompt.toLowerCase();
+            if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
+                keepGoing = false;
+            } else {
+                alert('Please enter "Rock", "Paper" or "Scissors".');
+            }
+        } else {
+            alert('Please enter "Rock", "Paper" or "Scissors".');
+        }
+    }
+    return playerChoice;
+}
+
+function getComputerChoice() {
+    let choice;
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        choice = "rock";
+    } else if (randomNumber === 1) {
+        choice = "paper";
+    } else if (randomNumber === 2) {
+        choice = "scissors";
+    }
+    return choice;
+}
+
+function playRound(playerChoice, computerChoice) {
+    
+}
+
+
+const playerChoice = getPlayerChoice();
+const computerChoice = getComputerChoice();
+
+console.log(playRound(playerChoice, computerChoice));
