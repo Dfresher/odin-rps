55102e0f81e51519ee98f9862f77fe99e69cb6c8
Donfernando <121210665+Dfresher@users.noreply.github.com>
2024-05-07 21:56:06 -0400

    Add UI and overhaul functionality
    Technically not finished, but I can't for the life
    of me figure out how to remove event listeners
    from anonymous functions... without completely
    breaking everything else, so it'll stay like this
    for a while.

--- script.js
+++ script.js
@@ -1,71 +1,58 @@
-function getPlayerChoice() {
-    keepGoing = true;
-    let playerChoice
-    while (keepGoing) {
-        let playerPrompt = prompt("Rock, Paper or Scissors?");
-        if (typeof(playerPrompt) === "string" || playerPrompt instanceof String) {
-            playerChoice = playerPrompt.toLowerCase();
-            if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-                keepGoing = false;
-            } else {
-                alert('Please enter "Rock", "Paper" or "Scissors".');
-            }
-        } else {
-            alert('Please enter "Rock", "Paper" or "Scissors".');
-        }
-    }
-    return playerChoice;
-}
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.textContent)
+    });
+});
+
+let player_score = 0;
+let computer_score = 0;
 
 function getComputerChoice() {
     let choice;
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
-        choice = "rock";
+        choice = "Rock";
     } else if (randomNumber === 1) {
-        choice = "paper";
+        choice = "Paper";
     } else if (randomNumber === 2) {
-        choice = "scissors";
+        choice = "Scissors";
     }
     return choice;
 }
 
-function playRound(playerChoice, computerChoice) {
-    let result;
-    if (playerChoice === 'rock' && computerChoice === 'scissors' || playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'scissors' && computerChoice === 'paper') {
-        console.log('The winner of this round is the player!') ;
-        result = 'player';
+
+function playRound(playerChoice) {
+    const computer_choice = document.querySelector(".computer-choice");
+    let computerChoice = getComputerChoice();
+    computer_choice.textContent = `The computer chose ${computerChoice}.`;
+
+    const round_winner = document.querySelector('.round-winner');
+    const scores = document.querySelector('.scores');
+    if (playerChoice === 'Rock' && computerChoice === 'Scissors' || playerChoice === 'Paper' && computerChoice === 'Rock' || playerChoice === 'Scissors' && computerChoice === 'Paper') {
+        round_winner.textContent = 'The winner of this round is the player!';
+        player_score += 1;
     } else if (playerChoice === computerChoice) {
-        console.log('This round ends in a draw!')
-        result = 'draw';
-    } else {
-        console.log('The winner of this round is the computer!')
-        result = 'computer';
-    }
-    return result;
-}
+        round_winner.textContent = 'This round ends in a draw!';
 
-function playGame() {
-    let roundWinner;
-    let winner = 0;
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        roundWinner = playRound(playerChoice, computerChoice);
-        if (roundWinner === 'player') {
-            winner++;
-        } else if (roundWinner === 'computer') {
-            winner--;
-        }
-    }
-
-    if (winner > 0) {
-        console.log('THE FINAL WINNER IS THE PLAYER!!!');
-    } else if (winner < 0) {
-        console.log('THE FINAL WINNER IS THE COMPUTER!!!');
     } else {
-        console.log('THE GAME ENDS IN A DRAW')
+        round_winner.textContent = 'The winner of this round is the computer!';
+        computer_score += 1;
+    }
+    scores.textContent = `Player score: ${player_score}, Computer score: ${computer_score}.`;
+
+    if (computer_score == 5 ||  player_score == 5) {
+        endGame();
     }
 }
 
-playGame();
+function endGame() {
+    const winner = document.querySelector('.winner');
+    if (player_score == 5) {
+        winner.textContent = 'THE FINAL WINNER IS THE PLAYER!!!';
+    } else if (computer_score == 5) {
+        winner.textContent = 'THE FINAL WINNER IS THE COMPUTER!!!';
+    } else {
+        winner.textContent = 'THE GAME ENDS IN A DRAW';
+    }
+}
